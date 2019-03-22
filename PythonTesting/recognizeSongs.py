95d46534c18cfe27dd720fe821f62bbf68829246
Dynamitelaw <noobeater122333444455555@gmail.com>
2019-03-22 04:35:14 -0400

    Classification somewhat working

--- PythonTesting/recognizeSongs.py
+++ PythonTesting/recognizeSongs.py
@@ -1,23 +1,29 @@
 
-#import the pyplot and wavfile modules 
+################################
+'''
+This script is for testing the functionality of our recognition algorithm.
+Algorithm and implimentation is based on this article: http://coding-geek.com/how-shazam-works/
+'''
+################################
 
 import matplotlib.pyplot as plt
-
 from scipy.io import wavfile
 from scipy import signal
 import numpy as np
 from statistics import mean 
- 
+from statistics import mode
+import os
 
 
-def generateConstellationMap(audioFilePath, fftResolution=512):
+def generateConstellationMap(audioFilePath, fftResolution=512, downsampleFactor=1, sampleLength=False):
 	'''
 	Generates a constellation map for a given audio file.
-	Method based on this article: http://coding-geek.com/how-shazam-works/
 
 	Parameters:
 		<string> audioFilePath: path to the input audio file. File must be a mono wave file with no metadata
 		<int> fftResolution: number of frequencies to sample over (power of 2). Defaults to 512
+		<int> downsampleFactor: factor by which to downsample the timesteps of the output. Defaults to 1
+		<float> sampleLength: If defined, creates a constellation map only for the first N seconds of the recording. Defaults to false. Must be less than the length of the recording
 
 	Returns:
 		(times, peakFreaquencies)
@@ -33,6 +39,9 @@ def generateConstellationMap(audioFilePath, fftResolution=512):
 	#Generate spectogram
 	frequencies, times, spectrogramData = signal.spectrogram(signalData, samplingFrequency, nperseg=fftResolution)
 	spectrogramData = np.transpose(spectrogramData)  #transpose data, so each row is a single sample
+	if (sampleLength):
+		spectrogramData = spectrogramData[0:int(sampleLength*samplingFrequency)]
+		times = times[0:int(sampleLength*samplingFrequency)]
 
 	#Bin frequencies in logarithmic bands
 	numberOfBins = 6
@@ -43,8 +52,10 @@ def generateConstellationMap(audioFilePath, fftResolution=512):
 	bins = [0] + bins
 
 	#Find the peak frequencies in the spectogram
+	returnTimes = []
 	peakFrequencies = []
-	for sample in spectrogramData:
+	for s in range(0, len(spectrogramData), downsampleFactor):
+		sample = spectrogramData[s]
 		#Find the peaks for each frequency bin
 		binPeaks = []
 		for b in range(0, numberOfBins, 1):
@@ -61,7 +72,7 @@ def generateConstellationMap(audioFilePath, fftResolution=512):
 					peakIndex = index
 
 			#Translate index into frequency, then append to samplePeaks list
-			binPeaks.append((frequencies[peakIndex], currentAmplitude))
+			binPeaks.append((frequencies[bins[b]:bins[b+1]][peakIndex], currentAmplitude))
 
 		#Determine which peaks in which bins to keep
 		averageAmplitude = sum([i[1] for i in binPeaks])/numberOfBins
@@ -71,24 +82,101 @@ def generateConstellationMap(audioFilePath, fftResolution=512):
 				filteredFrequencyPeaks.append(fBin[0])  #Include bin peak if it's amplitude is larger than the average amplitude of the bin peaks for this sample
 		
 		peakFrequencies.append(filteredFrequencyPeaks)
+		returnTimes.append(times[s])
 	
-	return times, peakFrequencies
+	return returnTimes, peakFrequencies
 
 
 
-def generateFingerprints(times, peakFrequencies, fingerPrintLength=1, samplingFrequency=44100):
+def generateFingerprints(times, peakFrequencies, songID):
 	'''
-	Generates fingerprints from a constellation map. Each fingerprint
+	Generates fingerprints from a constellation map.
+	Returns fingerprints as a list.
 	'''
-	pass
+
+	#Reorganize constellation inputs into a 1D list of ordered points (frequency, time)
+	points = []
+	for i in range(0, len(peakFrequencies), 1):
+		peaks = peakFrequencies[i]
+		for p in peaks:
+			points.append((p, times[i]))
+
+	#Create target zones
+	targetZoneSize = 5  #number of points to include in each target zone 
+	targetZones = []
+	for i in range(0, len(points)-targetZoneSize, 1):
+		targetZones.append(points[i:i+targetZoneSize])
+
+	#Generate fingerprints
+	anchorPointSeperation = 5  #How far back the anchor point is for each target zone (in terms of the order of the points)
+	fingerprints = []
+	for t in range(anchorPointSeperation, len(targetZones), 1):
+		targetZone = targetZones[t]
+		anchorPoint = targetZones[t - anchorPointSeperation][0]
+		anchorFrequency = anchorPoint[0]
+		anchorTimepoint = anchorPoint[1]
+
+		for point in targetZone:
+			pointFrequency = point[0]
+			timeDelta = point[1] - anchorTimepoint
+			pointFingerprint = (anchorFrequency, pointFrequency, timeDelta)
+			fingerprints.append((pointFingerprint,(songID, anchorTimepoint)))
+
+	return fingerprints
+	
+
+def identifySample(sampleFingerprints, hashTable):
+	'''
+	Identify the sample based on the fingerprints in the hashtable.
+	Returns the songID as a string.
+	'''
+	possibleMatches = []
+	for fingerprint in sampleFingerprints:
+		if fingerprint[0] in hashTable:
+			for songData in hashTable[fingerprint[0]]:
+				possibleMatches.append(songData[0])
+
+	if (len(possibleMatches)):
+		#print(possibleMatches)
+		return mode(possibleMatches)
+	else:
+		return "UNKNOWN"
 
 
+def main():
+	'''
+	Test our algorithm on the noisy sample tracks in the "InputFiles" folder.
+	Uses the songs in the "SongFiles" folder as the library to search against.
+	'''
+
+	#Generates a hash table (dictionary) for the song library, using fingerprints as the hash (key) and the songID and timepoints as the data points
+	print("____________________________________________")
+	print("Generating fingerprints for song library...")
+	hashTable = {}
+	songFileList = os.listdir("SongFiles")
+	for songFile in songFileList:
+		print("   " + songFile)
+		times, peaks = generateConstellationMap("SongFiles/"+songFile, downsampleFactor=4)
+		fingerprints = generateFingerprints(times, peaks, songFile.split("_")[0])
+
+		for fingperprint in fingerprints[:24]:
+			hashAddress = fingperprint[0]
+			songData = fingperprint[1]
+
+			if hashAddress in hashTable:  #fingerprint already exists
+				hashTable[hashAddress].append(songData)
+			else:
+				hashTable[hashAddress] = [songData]  #create new list of possible matches
+
+	#Try to identify noisy samples
+	print("____________________________________________")
+	print("Identifying samples...")
+	songFileList = os.listdir("InputFiles")
+	for songFile in songFileList:
+		times, peaks = generateConstellationMap("InputFiles/"+songFile, downsampleFactor=4, sampleLength=20)  #try to identify on first 20 seconds of sample
+		sampleFingerprints = generateFingerprints(times, peaks, songFile.split("_")[0])
+		songID = identifySample(sampleFingerprints, hashTable)
+		print("   " + songFile + " => " + songID)
 
 
-filepath = 'SongFiles/Marble Machine_by Wintergatan.wav'
-times, peaks = generateConstellationMap(filepath)
-
-print(peaks[50:53])
-
-#plt.plot(times, peaks)
-#plt.show()
+main()
