341d9d2c613bf4e3acd5e5527fe73482c4c586c0
Eitan Kaplan <ek2928@columbia.edu>
2019-04-09 16:09:02 -0400

    global variables

---
+++ Hardware/global_variables.sv
@@ -0,0 +1,8 @@
+`define NFFT 256 // if change this, change FREQ_WIDTH
+`define FREQS (`NFFT / 2)
+`define FREQ_WIDTH 7 // if change NFFT, change this
+
+`define FINAL_AMPL_WIDTH 24
+`define INPUT_AMPL_WIDTH 24
+
+`define PEAKS 6 // Changing this requires many changes in code
