36a0270e3f258271e5d11f6ab216667303859504
Dynamitelaw <noobeater122333444455555@gmail.com>
2019-04-11 18:20:13 -0400

    Finished FFT matlab testing. Almost done with input shuffling

--- MatlabTesting/FFT_Testing.m
+++ MatlabTesting/FFT_Testing.m
@@ -1,8 +1,20 @@
-a = [11, 85, 23, 33, 6, 90, 77, 61];
-%a = [11, 85];
-Y = fft(a);
+%a = [11, 85, 23, 33, 6, 90, 77, 61];
+a = [0, 1, 2, 3, 4, 5, 6, 7];
+shuffleInput(a);
+%a = [11, 85, 23, 33];
+%a = [1: 2^5]
+%Y = fft(a);
 
-disp(Y);
+%disp(real(Y));
 %Z = myButterfly(a, 0, 2);
 %disp(Z);
-myFFT(a);
+%Z = myFFT([a(0+1), a(2+1), a(1+1), a(3+1)]);
+%Z = myFFT(a);
+%disp(Z);
+
+%module = 1;
+%stages = 5;
+%stage = 1;
+%de2bi(2*module, stages)
+%bi2de(circshift(de2bi(2*module, stages), 1*stage))
+%bi2de(circshift(de2bi(2*module+1, stages), 1*stage))
--- MatlabTesting/myButterfly.m
+++ MatlabTesting/myButterfly.m
@@ -1,6 +1,10 @@
 function bufferfly = myButterfly(x, k, N)
     bufferfly = [0, 0];
-    w = exp(-2*pi*i*k/N);
+    w = exp(-2i*pi*k/N);
+    w = real(w);
     bufferfly(1) = x(1) + w*x(2);
     bufferfly(2) = x(1) - w*x(2);
+    
+    %bufferfly(1) = x(2);
+    %bufferfly(2) = x(1);
 end
