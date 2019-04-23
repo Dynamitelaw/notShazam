74aa7cd1ff9ef054a414f4829f67917a0a07258f
Dynamitelaw <noobeater122333444455555@gmail.com>
2019-04-23 15:45:16 -0400

    Let's try this again

--- Hardware/updateBootPartition.sh
+++ Hardware/updateBootPartition.sh
@@ -8,7 +8,7 @@ mount /dev/mmcblk0p1 /mnt
 #Copy over rbf and and dtb files
 echo "#############################"
 cp output_files/soc_system.rbf /mnt/soc_system.rbf
-cp output_files/soc_system.dtb /mnt/soc_system.dtb
+cp soc_system.dtb /mnt/soc_system.dtb
 
 #Reboot
 reboot
