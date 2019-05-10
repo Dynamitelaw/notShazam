7a16640978daec06573ad693f7ef5cbca9f9a04c
unownl <tep2116@columbia.edu>
2019-05-10 18:53:16 -0400

    debug3

--- driver/recognize.cpp
+++ driver/recognize.cpp
@@ -630,9 +630,10 @@ uint64_t get_sample(std::vector<float> & fft) {
 		return ERR_NVALID;
 	}
 	for (int i = 0; i < N_FREQUENCIES; i++) {
-		std::cout << fft_struct.fft[i] << std::endl;
+		std::cout << fft_struct.fft[i] << " ";
 		fft.push_back(ampl2float(fft_struct.fft[i]));
 	}
+		std::cout << std::endl;
 	return fft_struct.time;
 }
 
