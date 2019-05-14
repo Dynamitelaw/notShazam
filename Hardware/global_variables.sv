56c28a0f10b8fe2ba2c310144b98a67675ba55b8
Dynamitelaw <noobeater122333444455555@gmail.com>
2019-05-13 22:47:12 -0400

    Fixed test wave frequency. Set sampling frequency back to default

--- Hardware/global_variables.sv
+++ Hardware/global_variables.sv
@@ -32,7 +32,7 @@
 `define AUDIO_IN_GAIN 9'h010
 `define AUDIO_OUT_GAIN 9'h061
 
-`define SAMPLE_RATE_CNTRL 9'b0000_11000  //see page 44 of datasheet of more info: https://statics.cirrus.com/pubs/proDatasheet/WM8731_v4.9.pdf
+`define SAMPLE_RATE_CNTRL 9'b0000_00000  //see page 44 of datasheet of more info: https://statics.cirrus.com/pubs/proDatasheet/WM8731_v4.9.pdf
 
 /*
 // BINS NFFT=16
