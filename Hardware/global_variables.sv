6cd660b2a9873a2be405c25f0fb13c364361e381
Dynamitelaw <noobeater122333444455555@gmail.com>
2019-05-14 15:10:06 -0400

    Fixed global variables bug

--- Hardware/global_variables.sv
+++ Hardware/global_variables.sv
@@ -17,7 +17,7 @@
 `define SFFT_INPUT_WIDTH 24
 `define SFFT_OUTPUT_WIDTH `INPUT_AMPL_WIDTH
 `define SFFT_FIXEDPOINT_INPUTSCALING  //define this macro if you want to scale adc inputs to match FixedPoint magnitudes. Increases accuracy, but could lead to overflow
-`define SFFT_FIXED_POINT_ACCURACY 7 
+`define SFFT_FIXED_POINT_ACCURACY 7 //asd
 `define SFFT_STAGECOUNTER_WIDTH 5  //>= log2(nFFT)
 
 //`define SFFT_DOWNSAMPLE_PRE  //define this macro if you want to downsample the incoming audio BEFORE the FFT calculation
